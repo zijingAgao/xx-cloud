842a3e21ce3686270c7757a3a3c4f5e3a820cf77
gaoyl <2562140443@qq.com>
2024-03-18 17:23:40 +0800

    feat: ip寻址暴露feign接口

--- xx-cloud-ip-addr/ip-addr-feign/src/main/java/com/xx/feign/IpAddrService.java
+++ xx-cloud-ip-addr/ip-addr-feign/src/main/java/com/xx/feign/IpAddrService.java
@@ -1,8 +1,12 @@
 package com.xx.feign;
 
+import com.xx.vo.IpAddrVo;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @FeignClient("xx-cloud-ip-addr")
 public interface IpAddrService {
-
+  @GetMapping("/api/ipaddr/{ip}")
+  IpAddrVo ipAddr(@PathVariable("ip") String ip);
 }
